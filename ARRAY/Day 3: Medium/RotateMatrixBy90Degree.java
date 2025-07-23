f81f0882cc795aab43ce7fd39f412bf365053a7f
shivesh9922-blip <shivesh9922@gmail.com>
2025-07-23 14:58:00 +0530

    Update RotateMatrixBy90Degree.java

--- ARRAY/Day 3: Medium/RotateMatrixBy90Degree.java
+++ ARRAY/Day 3: Medium/RotateMatrixBy90Degree.java
@@ -1,27 +1,27 @@
 class RotateMatrixBy90Degree {
 public static void rotate(int[][] matrix)
-{ //row length
-int n=matrix.length;
-//coloumn length
-int m=matrix[0].length;
-for(int i=0;i<n;i++)
-{
-for(int j=i;j<m;j++)
-{
-int temp=matrix[i][j];
-matrix[i][j]=matrix[j][i];
-matrix[j][i]=temp;
-}
-}
-for(int i=0;i<n;i++)
-{
-for(int j=0;j<n/2;j++)
-{
-int temp=matrix[i][j];
-matrix[i][j]=matrix[i][n-1-j];
-matrix[i][n-1-j]=temp;
-}
-}
+{ 
+   //row
+    int n = matrix.length;
+        // Perform Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int start = 0, end = n - 1;
+            while (start < end) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
 }
 public static void main(String args[]) {
 int arr[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
