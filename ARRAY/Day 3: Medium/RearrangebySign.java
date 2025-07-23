21c90217b3f86c6609d5b6e4d0abec21e7726a98
shivesh9922-blip <shivesh9922@gmail.com>
2025-07-23 14:27:18 +0530

    Update RearrangebySign.java

--- ARRAY/Day 3: Medium/RearrangebySign.java
+++ ARRAY/Day 3: Medium/RearrangebySign.java
@@ -1,30 +1,24 @@
-import java.util.*;
 class RearrangebySign {
-public static ArrayList<Integer> reArrangeBySign(ArrayList<Integer> alist){
-int n=alist.size();
-ArrayList<Integer> ans=new ArrayList<>(Collections.nCopies(n,0));
-int posIndex=0;
-int negIndex=1;
-for(int i=0;i<n;i++)
+    public static int[] reArrangeBySign(int[] nums) {
+          int[] rearrangedArray = new int[nums.length];
+        int positiveIndex = 0, negativeIndex = 1;
+        for (int num : nums) {
+            if (num > 0) {
+              rearrangedArray[positiveIndex] = num;
+                positiveIndex += 2; 
+            } else {
+               rearrangedArray[negativeIndex] = num;
+                negativeIndex += 2; 
+            }
+        }
+        return rearrangedArray;
+    }
+    public static void main(String[] args) {
+int[] nums={3,1,-2,-5,2,-4};
+int[] ans = reArrangeBySign(nums);
+for (int i = 0; i < ans.length; i++)
 {
-if(alist.get(i)<0)
-{
-ans.set(negIndex,alist.get(i));
-negIndex+=2;
-}
-else
-{
-ans.set(posIndex,alist.get(i));
-posIndex+=2;
-}
-}
-return ans;
-}
-public static void main(String[] args) {
-ArrayList<Integer> A = new ArrayList<>(Arrays.asList(1, 2, -4, -5));
-ArrayList<Integer> ans = reArrangeBySign(A);
-for (int i = 0; i < ans.size(); i++) {
-System.out.print(ans.get(i) + " ");
-}
+    System.out.print(ans[i] + " ");
+      }
 }
 }
