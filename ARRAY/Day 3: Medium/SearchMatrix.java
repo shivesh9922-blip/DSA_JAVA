4177b1a03501a74c6269f7dcb5007750bc85287d
shivesh9922-blip <shivesh9922@gmail.com>
2025-07-23 11:50:58 +0530

    Update SearchMatrix.java

--- ARRAY/Day 3: Medium/SearchMatrix.java
+++ ARRAY/Day 3: Medium/SearchMatrix.java
@@ -1,10 +1,11 @@
 import java.util.*;
 class SearchMatrix {
-public static boolean searchMatrix(ArrayList<ArrayList<Integer>> matrix, int target)
-{//Row
-int n=matrix.size();
+public static boolean searchMatrix(int[][] matrix, int target)
+{
+//Row
+int n=matrix.length;
 //Coulumn
-int m=matrix.get(0).size();
+int m=matrix[0].length;
 int low=0;
 int high=n*m-1;
 while(low<=high)
@@ -14,11 +15,11 @@ int mid=(low+high)/2;
 int row=mid/m;
 //col=mid%column
 int col=mid%m;
-if(matrix.get(row).get(col)==target)
+if(matrix[row][col]==target)
 {
 return true;
 }
-else if(matrix.get(row).get(col)<target)
+else if(matrix[row][col]<target)
 {
 low=mid+1;
 }
@@ -28,10 +29,11 @@ high=mid-1;
 return false;
 }
 public static void main(String[] args) {
-ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
-matrix.add(new ArrayList<>(Arrays.asList(1, 2, 3, 4)));
-matrix.add(new ArrayList<>(Arrays.asList(5, 6, 7, 8)));
-matrix.add(new ArrayList<>(Arrays.asList(9, 10, 11, 12)));
+      int[][] matrix = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
 boolean result = searchMatrix(matrix, 8);
 System.out.println(result ? "true" : "false");
 }
